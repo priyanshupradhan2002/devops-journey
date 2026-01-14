dd9eadaf38ebf4989a4c921ca817ded3b76eccf7
priyanshupradhan2002 <priyanshupradhan2002@gmail.com>
2026-01-14 20:26:00 +0530

    For loop execution

---
+++ for_loop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#######################
+#Author:- Priyanshu Pradhan
+#date:- 14th Jan 2026
+#We are trying to execute a for loop here
+#######################
+
+
+#For loop Execution
+for i in {1..10}
+do
+ echo "$i. Priyanshu Pradhan"
+done
